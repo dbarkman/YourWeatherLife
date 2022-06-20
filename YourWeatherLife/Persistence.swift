7b8dacfc5a287f5bb85a39e656d74f58949e3fec
David Barkman <david.barkman13@gmail.com>
2022-06-20 10:53:08 -0700

    Updated PersistanceController. Other misc updates.

--- YourWeatherLife/Persistence.swift
+++ YourWeatherLife/Persistence.swift
@@ -7,11 +7,11 @@
 
 import CoreData
 
-struct PersistenceController {
-  static var shared = PersistenceController()
+class PersistenceController {
+  static let shared = PersistenceController()
   
-  static var preview: PersistenceController = {
-    var result = PersistenceController(inMemory: true)
+  static let preview: PersistenceController = {
+    let result = PersistenceController(inMemory: true)
     let viewContext = result.container.viewContext
     for _ in 0..<10 {
       let newItem = Item(context: viewContext)
@@ -25,30 +25,36 @@ struct PersistenceController {
     }
     return result
   }()
-  
-  lazy var container = NSPersistentCloudKitContainer(name: "YourWeatherLife")
-  
-  init(inMemory: Bool = false) {
-    //    container = NSPersistentCloudKitContainer(name: "YourWeatherLife")
-    if inMemory {
-      container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+
+  private let inMemory: Bool
+
+  lazy var container: NSPersistentCloudKitContainer = {
+    
+    let container = NSPersistentCloudKitContainer(name: "YourWeatherLife")
+    
+    guard let description = container.persistentStoreDescriptions.first else {
+      fatalError("Failed to retrieve a persistent store description.")
     }
-    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+
+    let storesURL = description.url?.deletingLastPathComponent()
+    description.url = storesURL?.appendingPathComponent("private.sqlite")
+
+    if inMemory {
+      description.url = URL(fileURLWithPath: "/dev/null")
+    }
+    
+    container.loadPersistentStores { storeDescription, error in
       if let error = error as NSError? {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        
-        /*
-         Typical reasons for an error here include:
-         * The parent directory does not exist, cannot be created, or disallows writing.
-         * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-         * The device is out of space.
-         * The store could not be migrated to the current model version.
-         Check the error message to determine what the actual problem was.
-         */
         fatalError("Unresolved error \(error), \(error.userInfo)")
       }
-    })
+    }
+    
+    container.viewContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
     container.viewContext.automaticallyMergesChangesFromParent = true
+    return container
+  }()
+  
+  init(inMemory: Bool = false) {
+    self.inMemory = inMemory
   }
 }
