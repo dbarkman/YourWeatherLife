fec9f42da16afc1e7804def7b6956a3c790f14c4
David Barkman <david.barkman13@gmail.com>
2022-06-20 09:56:37 -0700

    Started project, reformatted all code. Working on CoreData Container.

--- YourWeatherLife/YourWeatherLifeApp.swift
+++ YourWeatherLife/YourWeatherLifeApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct YourWeatherLifeApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
     }
+  }
 }
