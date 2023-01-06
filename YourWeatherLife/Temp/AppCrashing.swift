41b8d9ed7761432604e82da44635675cd10f7198
David Barkman <david.barkman13@gmail.com>
2023-01-05 22:11:10 -0700

    AppCrashing screen update.

--- YourWeatherLife/Temp/AppCrashing.swift
+++ YourWeatherLife/Temp/AppCrashing.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Mixpanel
 
 struct AppCrashing: View {
     var body: some View {
@@ -14,15 +15,16 @@ struct AppCrashing: View {
           Text("Sorry About the Crashes")
             .font(.title)
           Text("If you experienced crashing with the January 3rd, 2023 release of the app, your database likely has some corrupt data.")
-            .lineLimit(nil)
             .padding(.top, 5)
-          Text("This was likely caused by me having to change weather data providers or just by the developer 😬 going so long between updates.")
+          Text("This was likely caused by me having to change weather data providers or just by the developer going so long between updates. 😬")
             .padding(.top, 5)
-          Text("The best corse of action at this point, is to completely remove the app from your device, then reinstall it from TestFlight.")
+          Text("The best corse of action at this point, is to remove the app from your device, then reinstall the app from TestFlight.")
+            .padding(.top, 5)
+          Text("Any daily events you created should be backed up in iCloud and should reappear when you reinstall the app. This should be a one-time event, updating through TestFlight should be fine for future versions.")
             .padding(.top, 5)
           Text("Once you perform a clean \"Install\" from TestFlight, this screen will no longer appear and crashing caused by databases should cease.")
             .padding(.top, 5)
-          Text("This app is in active development and may crash for other reasons, lol. 😀")
+          Text("This app is in active development and may crash for other reasons, lol. 😆")
             .padding(.top, 5)
           Text("I will try to be more careful in the future, thank you so much for taking the time to test and use this app!")
             .padding(.top, 5)
@@ -31,6 +33,9 @@ struct AppCrashing: View {
           Spacer()
         }
         .padding()
+        .onAppear() {
+          Mixpanel.mainInstance().track(event: "AppCrashing View")
+        }
       }
     }
 }
