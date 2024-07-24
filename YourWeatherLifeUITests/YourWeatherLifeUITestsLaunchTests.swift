129fa4ae04ca209d6b543fff37b718c8a296aa51
David Barkman <david.barkman13@gmail.com>
2024-07-23 22:12:13 -0700

    added a test

--- YourWeatherLifeUITests/YourWeatherLifeUITestsLaunchTests.swift
+++ YourWeatherLifeUITests/YourWeatherLifeUITestsLaunchTests.swift
@@ -9,24 +9,31 @@ import XCTest
 
 class YourWeatherLifeUITestsLaunchTests: XCTestCase {
   
-//  override class var runsForEachTargetApplicationUIConfiguration: Bool {
-//    true
-//  }
-//  
-//  override func setUpWithError() throws {
-//    continueAfterFailure = false
-//  }
-//  
-//  func testLaunch() throws {
-//    let app = XCUIApplication()
-//    app.launch()
-//    
-//    // Insert steps here to perform after app launch but before taking a screenshot,
-//    // such as logging into a test account or navigating somewhere in the app
-//    
-//    let attachment = XCTAttachment(screenshot: app.screenshot())
-//    attachment.name = "Launch Screen"
-//    attachment.lifetime = .keepAlways
-//    add(attachment)
+  let app = XCUIApplication()
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+    app.launch()
+  }
+  
+  override func tearDownWithError() throws {
+    app.terminate()
+    try super.tearDownWithError()
+  }
+  
+  func testNavigation() {
+    let weekendPredicate = NSPredicate(format: "label beginswith 'Weekend'")
+    app.tabBars.buttons.element(matching: weekendPredicate).tap()
+    XCTAssert(app.staticTexts["This Weekend"].exists)
+  }
+  
+//  func testLaunchPerformance() throws {
+//    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//      // This measures how long it takes to launch your application.
+//      measure(metrics: [XCTApplicationLaunchMetric()]) {
+//        XCUIApplication().launch()
+//      }
+//    }
 //  }
 }
